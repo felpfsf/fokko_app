66efdab671cc95f60ef55f2c34e47f5766ba021a
Felipe Ferreira <feferreira.sf@gmail.com>
2025-07-09 15:33:14 -0300

    ✨ Update main function to initialize theme controller asynchronously

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fokko/core/utils/theme_controller.dart';
 
 import 'app.dart';
 import 'flavors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   F.appFlavor = Flavor.values.firstWhere(
     (element) => element.name == appFlavor,
   );
+
+  await ThemeModeController.init();
 
   runApp(const App());
 }
